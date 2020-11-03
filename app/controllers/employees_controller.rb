eef025458fcaf6dab2bfa3e621f98349c6a713fb
jpeterson14 <jpeterson0514@gmail.com>
2020-11-03 14:42:02 -0600

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,31 @@
 class EmployeesController < ApplicationController
+def index
+    @employees = Employee.all
+end
+def new
+    @employee = Employee.new
+end
+def create
+    strong_params = params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+    @employee = Employee.create(strong_params)
+    redirect_to employee_path(@employee)
+    
+end
+
+def show
+    @employee = Employee.find_by(id: params[:id])
+end
+
+def edit
+    @employee = Employee.find_by(id: params[:id])
+end
+
+def update
+    @employee = Employee.find_by(id: params[:id])
+    strong_params = params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+    @author = Employee.create(strong_params)
+    redirect_to employees_path
+end
+
+
 end
